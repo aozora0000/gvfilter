da64f6aa3bd9f6339e8b0cffb58d690c55ca613a
aozora0000 <aozora0000@gmail.com>
2022-01-20 16:11:42 +0900

    空文字を指定するとそのまま表示

--- main.go
+++ main.go
@@ -12,6 +12,20 @@ func main() {
 	if len(os.Args) < 1 {
 		panic(fmt.Errorf("argument invalid"))
 	}
+	if os.Args[1] == "" {
+		var versions []*semver.Version
+		buf := bufio.NewScanner(os.Stdin)
+		for buf.Scan() {
+			v, err := semver.NewVersion(buf.Text())
+			if err == nil {
+				versions = append(versions, v)
+			}
+		}
+		sort.Sort(semver.Collection(versions))
+		for _, v := range versions {
+			fmt.Println(v.String())
+		}
+	}
 	if os.Args[1] == "latest" {
 		var versions []*semver.Version
 		buf := bufio.NewScanner(os.Stdin)
